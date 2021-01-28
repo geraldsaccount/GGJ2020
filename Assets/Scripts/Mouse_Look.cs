4022e38ccbec127df1312a4fdb4aa6da81528758
JuuzouBLAACK <immanuel.buecking247@gmail.com>
2021-01-28 02:06:04 +0100

    [ADDED] Player Controller Prototype & Test Scenes

---
+++ Assets/Scripts/Mouse_Look.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Look : MonoBehaviour
+{
+
+    public float MouseX;
+    public float MouseY;
+    public float MouseSensitivity = 100f;
+    private float xRotation = 0f;
+
+    public Transform playerBody;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * MouseX);
+    }
+}
